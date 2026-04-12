6fddcf1052a112d5ef81382fedb3edaf82c5a9b3
iTovarr <13m.tovar.m@gmail.com>
2026-04-12 18:13:13 -0500

    feat: add reusable Card component from previous lab

--- app/src/main/java/com/example/laboratorio4/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio4/MainActivity.kt
@@ -12,6 +12,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.laboratorio4.ui.theme.Laboratorio4Theme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +28,15 @@ class MainActivity : ComponentActivity() {
         setContent {
             Laboratorio4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Tarjeta(
+                        titulo = "Componente 1",
+                        desc = "Contenedor con bordes y color.",
+                        color = Color(0xFF1A1A1A),
+                        accentColor = Color(0xFF00E5FF),
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        Text("Ya estás dentro de una Card", color = Color.White)
+                    }
                 }
             }
         }
@@ -31,17 +44,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Laboratorio4Theme {
-        Greeting("Android")
+fun Tarjeta(titulo: String, desc: String, color: Color, accentColor: Color, modifier: Modifier = Modifier, contenido: @Composable () -> Unit) {
+    Card(
+        modifier = modifier.fillMaxWidth().padding(16.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(containerColor = color)
+    ) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            Text(titulo, fontWeight = FontWeight.Bold, fontSize = 20.sp, color = accentColor)
+            Text(desc, fontSize = 12.sp, color = Color.Gray)
+            Spacer(modifier = Modifier.height(16.dp))
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
+                contenido()
+            }
+        }
     }
 }
